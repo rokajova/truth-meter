6d69daa8599cac775be76d19d1a4b6da5df1a70e
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-12-22 22:04:08 +0200

    Viewpost Props

--- src/Component/PostCard/PostCard.js
+++ src/Component/PostCard/PostCard.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const PostCard = (props) => {
   return (
     <div style={{ border: "1px solid black", margin: "10px" }}>
-      <Link to={"post/" + props.data.id}>
+      <Link
+        to={{ pathname: "post/" + props.data.id, state: { post: props.data } }}
+      >
         <h1>{props.data.title}</h1>
         <a href={props.data.link}>{props.data.link}</a>
       </Link>
--- src/Component/ViewPost/ViewPost.js
+++ src/Component/ViewPost/ViewPost.js
@@ -1,14 +1,16 @@
 import { ReactReduxContext } from "react-redux";
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 class ViewPost extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    console.log(this.props);
   }
   render() {
     return <div>Post</div>;
   }
 }
 
-export default ViewPost;
+export default withRouter(ViewPost);
