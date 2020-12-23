24052bc54ba790471e6619d9aa1a99839d38f50a
rokajova <35878122+RockyJov@users.noreply.github.com>
2020-12-23 11:23:49 +0200

    viewpost

--- src/Component/ViewPost/ViewPost.js
+++ src/Component/ViewPost/ViewPost.js
@@ -5,11 +5,38 @@ import { withRouter } from "react-router-dom";
 class ViewPost extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      post: {},
+      isLoaded: false,
+    };
     console.log(this.props);
   }
+
+  componentDidMount() {
+    if (typeof this.props.location.state !== "undefined") {
+      if (this.props.location.state.hasOwnProperty("post")) {
+        this.setState(
+          {
+            post: this.props.location.state.post,
+          },
+          () => {
+            this.setState({ isLoaded: true });
+          }
+        );
+      }
+    } else {
+      this.getPostByID(this.props.match.params.id);
+    }
+  }
+
+  getPostByID() {}
+
   render() {
-    return <div>Post</div>;
+    if (this.state.isLoaded) {
+      return <div>{this.state.post.link}</div>;
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
