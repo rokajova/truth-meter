ada5e8106aba1a9534bdf93ef200f2294b3346a1
rokajova <35878122+RockyJov@users.noreply.github.com>
2021-01-08 10:34:38 +0200

    new user function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,13 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+// create a new collection in the firebase on new user create
+exports.newUserSignup = functions.auth.user().onCreate((user) => {
+  return admin.firestore().collection("Users").doc(user.uid).set({
+    userID: user.uid,
+    userName: user.displayName,
+    userPosts: [],
+    userRates: [],
+  });
 });
