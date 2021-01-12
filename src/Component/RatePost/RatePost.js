9af84973e8c2ecb1208a7875a3fc107dca7b59a0
rokajova <35878122+rokajova@users.noreply.github.com>
2021-01-12 14:03:24 +0200

    RatePost
    firebase update success

--- src/Component/RatePost/RatePost.js
+++ src/Component/RatePost/RatePost.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Input, Button } from "reactstrap";
-import firebase from "../../Config/firebase"
+import firebase from "../../Config/firebase";
 
-const db = firebase.firestore()
+const db = firebase.firestore();
 
 export default class RatePost extends Component {
   constructor(props) {
@@ -15,40 +15,56 @@ export default class RatePost extends Component {
     };
   }
 
-componentDidMount(){
-  console.log(this.props.history)
-}  
-
-  onSubmit = () =>{
-    // get user and post refs
-    const userRef = db.collection("Users").doc(this.props.auth.uid)
-    const postRef = db.collection("Posts").doc(this.props.location.state.post.id)
-    return userRef.onSnapshot((doc) => {
-      if(doc.data.userRates.includes(this.props.history.state.post.id)){
-        this.setState({
-          hasRated: true,
-        })
-      }
-      else userRef.update({userRates: [
-        ...doc.data().userRates,
-        this.props.location.state.post.id,
-        this.state.ratingscore
-      ]})
-    })
+  componentDidMount() {
+    console.log();
   }
+
+  onSubmit = () => {
+    // get user and post refs
+    const userRef = db.collection("Users").doc(this.props.auth.uid);
+
+    const postRef = db
+      .collection("Posts")
+      .doc(this.props.location.state.post.id);
+
+    return userRef.onSnapshot((doc) => {
+      if (doc.data().userRates.includes(this.props.location.state.post.id)) {
+        this.setState({ hasRated: true });
+        console.log("has this user rated? ", this.state.hasRated);
+      } else {
+        userRef.update({
+          userRates: [
+            ...doc.data().userRates,
+            this.props.location.state.post.id,
+          ],
+        });
+      }
+    });
+  };
 
   onChangeRateInput = (value) => {
-    this.setState({ratingScore: value})
-  }
+    this.setState({ ratingScore: value });
+  };
 
   render() {
     return (
       <div>
-        <div style={{textAlign: "center"}}>{this.state.ratingScore}</div>
-        
-        <Input type="range" min="0" max="100" value={this.state.ratingScore} onChange={(e) => this.onChangeRateInput(e.target.value)}/>
-        {this.state.hasRated?<Button disabled>Submit</Button> : <Button color="success" onClick={()=> this.onSubmit()}>Submit</Button> }
-        
+        <div style={{ textAlign: "center" }}>{this.state.ratingScore}</div>
+
+        <Input
+          type="range"
+          min="0"
+          max="100"
+          value={this.state.ratingScore}
+          onChange={(e) => this.onChangeRateInput(e.target.value)}
+        />
+        {this.state.hasRated ? (
+          <Button disabled>Submit</Button>
+        ) : (
+          <Button color="success" onClick={() => this.onSubmit()}>
+            Submit
+          </Button>
+        )}
       </div>
     );
   }
