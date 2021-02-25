32b28b95bddefc62d7215d1c9a1c39d8af7ebd90
rokajova <35878122+rokajova@users.noreply.github.com>
2021-02-25 15:01:04 +0200

    Header react-bootstrap

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -74,28 +74,45 @@ class Header extends Component {
       //     </UncontrolledDropdown>
       //   </Collapse>
       // </Navbar>
-      <Navbar bg="dark" expand="lg">
-        <Navbar.Brand href="#home">TruthMater</Navbar.Brand>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        </Form>
+      <Navbar bg="light" expand="md">
+        <Navbar.Brand href="/">TruthMeter</Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+          <Form inline>
+            <i className="fas fa-search mr-1" />
+            <FormControl type="text" className="mr-sm-2" />
+          </Form>
+          {this.props.auth.isEmpty ? (
+            <Nav className="ml-auto ">
+              <Nav.Link href="#">
+                &nbsp; <i className="fas fa-info" /> INFO
+              </Nav.Link>
+              <Nav.Link href="/login">
+                &nbsp; <i className="fas fa-user" /> LOG IN/SIGN UP
+              </Nav.Link>
+            </Nav>
+          ) : (
+            <Nav className="ml-auto">
+              <Nav.Link href="/new-post">
+                {" "}
+                &nbsp; <i className="fas fa-plus" />
+                NEW POST
+              </Nav.Link>
+              <NavDropdown title="USER" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/profile">My profile</NavDropdown.Item>
+
+                <NavDropdown.Divider />
+                <NavDropdown.Item
+                  onClick={() => {
+                    firebase.auth().signOut();
+                  }}
+                >
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Navbar>
     );
