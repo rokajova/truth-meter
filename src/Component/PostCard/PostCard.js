a9486a140aa5c72d8aaee63c51f1b333fb63d791
rokajova <35878122+rokajova@users.noreply.github.com>
2021-03-01 19:54:18 +0200

    Main bootstrap

--- src/Component/PostCard/PostCard.js
+++ src/Component/PostCard/PostCard.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container, Col, Row } from "react-bootstrap";
 import GaugeChart from "react-gauge-chart";
 import firebase from "../../Config/firebase";
+import classes from "./PostCard.module.css";
 
 const db = firebase.firestore();
 // Convert createDate timestamp seconds to string with result being YYYY/MM/DD HH:MM:SS
@@ -27,30 +29,26 @@ export function timeStampToString(ts) {
 
 const PostCard = (props) => {
   // gauge style
-  const chartStyle = { width: 400 };
 
   return (
     <Link
       to={{ pathname: "post/" + props.data.id, state: { post: props.data } }}
     >
-      <div style={{ border: "1px solid black", margin: "10px" }}>
-        <h1>{props.data.title}</h1>
-        <h3>{props.data.link}</h3>
-        <div>
-          Create Date : {timeStampToString(props.data.createDate.seconds)}
-        </div>
-        <div>Posted By: {props.data.createUserName}</div>
-        <div>RatingScore: {props.data.ratingScore} %</div>
-        <GaugeChart
-          style={chartStyle}
-          textColor="black"
-          id="gauge-chart6"
-          animate={false}
-          nrOfLevels={15}
-          percent={props.data.ratingScore / 100}
-          needleColor="#345243"
-        />
-      </div>
+      <Container className={classes.PostCardContainer}>
+        <Row>
+          <Col sm={8}>{props.data.link}</Col>
+          <Col sm={4}>
+            <GaugeChart
+              textColor="black"
+              id="gauge-chart6"
+              animate={false}
+              nrOfLevels={15}
+              percent={props.data.ratingScore / 100}
+              needleColor="#345243"
+            />
+          </Col>
+        </Row>
+      </Container>
     </Link>
   );
 };
