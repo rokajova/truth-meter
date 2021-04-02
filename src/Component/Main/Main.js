fe7ed67b211273354b4607e6d9cd914f66306230
rokajova <35878122+rokajova@users.noreply.github.com>
2021-04-02 06:44:34 +0300

    Main DONE

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import { Container, Spinner } from "react-bootstrap";
 import PostCard from "../PostCard/PostCard";
 import firebase from "../../Config/firebase";
 import classes from "./Main.module.css";
-import Search from "../Search/Search";
 
 const db = firebase.firestore();
 
@@ -12,44 +10,12 @@ class Main extends Component {
     super(props);
     this.myDiv = React.createRef();
     this.state = {
-      lastPost: null,
       isLoaded: false,
       posts: [],
-      showLoading: false,
+      showMoreDisabled: false,
+      lastPost: null,
     };
   }
-
-  nextPost = () => {
-    this.setState({ showLoading: true });
-    db.collection("Posts")
-      .orderBy("createDate", "desc")
-      .startAfter(this.state.lastPost.createDate)
-      .limit(1)
-      .get()
-      .then((docs) => {
-        if (!docs.empty) {
-          let allPosts = [];
-          docs.forEach(function (doc) {
-            const post = { id: doc.id, ...doc.data() };
-            allPosts.push(post);
-          });
-
-          let updated_posts = this.state.posts.concat(allPosts);
-          this.setState(
-            {
-              posts: updated_posts,
-            },
-            () => {
-              this.setState({
-                isLoaded: true,
-                lastPost: this.state.posts[this.state.posts.length - 1],
-              });
-            }
-          );
-        }
-        this.setState({ showLoading: false });
-      });
-  };
 
   componentDidMount() {
     this.getMyPosts();
@@ -70,6 +36,7 @@ class Main extends Component {
             };
             allPosts.push(post);
           });
+
           this.setState(
             {
               posts: allPosts,
@@ -85,37 +52,66 @@ class Main extends Component {
       });
   };
 
-  handleLoadMore = () => {
-    this.getMyPosts();
-  };
+  nextPost = () => {
+    this.setState({ showMoreDisabled: true });
+    db.collection("Posts")
+      .orderBy("createDate", "desc")
+      .startAfter(this.state.lastPost.createDate)
+      .limit(5)
+      .get()
+      .then((docs) => {
+        if (!docs.empty) {
+          let allPosts = [];
+          docs.forEach(function (doc) {
+            const post = { id: doc.id, ...doc.data() };
+            allPosts.push(post);
+          });
 
-  handleScroll = () => {
-    let triggerHeight =
-      this.myDiv.current.scrollTop + this.myDiv.current.offsetHeight;
-    if (triggerHeight >= this.myDiv.current.scrollHeight) {
-      this.nextPost();
-    }
+          let updated_posts = this.state.posts.concat(allPosts);
+
+          this.setState(
+            {
+              posts: updated_posts,
+            },
+            () => {
+              this.setState({
+                isLoaded: true,
+                lastPost: this.state.posts[this.state.posts.length - 1],
+                showMoreDisabled: false,
+              });
+            }
+          );
+        }
+      });
   };
 
   render() {
     return (
-      <div
-        className={classes.Container}
-        onScroll={() => this.handleScroll()}
-        ref={this.myDiv}
-      >
+      <div className={classes.Container}>
         {/* Once the posts array is populated, return the elements in the array as PostCard component with props */}
         {this.state.isLoaded &&
           this.state.posts.map((post, index) => {
             return <PostCard key={index} data={post} />;
           })}
-        {this.state.showLoading ? (
-          <div className={classes.Loading}>
-            <Spinner animation="grow" variant="light" />
-          </div>
-        ) : (
-          ""
-        )}
+        <div className={classes.ShowMore}>
+          {" "}
+          {this.state.showMoreDisabled ? (
+            <span className={classes.DownArrowDisabled}>
+              {" "}
+              <i class="fas fa-chevron-down" />
+            </span>
+          ) : (
+            <span
+              onClick={() => {
+                this.nextPost();
+              }}
+              className={classes.DownArrow}
+            >
+              {" "}
+              <i class="fas fa-chevron-down" />
+            </span>
+          )}
+        </div>
       </div>
     );
   }
